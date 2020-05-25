12795c025e74af9144a415bf208f1205ab6ed8fa
maxau <maxau@192.168.0.3>
2020-05-25 17:02:18 -0300

    Atividade

---
+++ src/atividadeslaa/ClienteMatrizAdjacencia.java
@@ -0,0 +1,54 @@
+package atividadeslaa;
+
+class Aresta {
+    int valor;
+    Aresta() {
+        this.valor = 0;
+    }
+}
+
+public class ClienteMatrizAdjacencia {
+
+    static int numeroVertices = 6;
+
+    public static void main(String[] args) {
+        Aresta[][] matriz = new Aresta[numeroVertices][numeroVertices];
+
+        ligarMatriz(matriz);
+
+        System.out.println("Sem aresta: ");
+        print(matriz);
+
+        (matriz[0][1]).valor = 1;
+        (matriz[0][5]).valor = 1;
+        (matriz[1][0]).valor = 1;
+        (matriz[1][5]).valor = 1;
+        (matriz[2][4]).valor = 1;
+        (matriz[3][1]).valor = 1;
+        (matriz[5][3]).valor = 1;
+
+        System.out.println("Com arestas: ");
+        print(matriz);
+
+    }
+
+    static void ligarMatriz(Aresta[][] matriz) {
+        for (int i = 0; i < numeroVertices; i++) {
+            for (int j = 0; j < numeroVertices; j++) {
+                matriz[i][j] = new Aresta();
+            }
+        }
+    }
+
+    static void print(Aresta[][] matriz) {
+        System.out.println("-  0 1 2 3 4 5 -");
+        for (int i = 0; i < numeroVertices; i++) {
+            System.out.print(i + ": ");
+            for (int j = 0; j < numeroVertices; j++) {
+                System.out.print(matriz[i][j].valor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}

b0b1d0db2fbbb78f1c16e5558ab94783efb4d17d
maxau <maxau@192.168.0.3>
2020-05-25 18:09:58 -0300

    Atividade

---
+++ src/atividadeslaa/GrafoTxt.java
@@ -0,0 +1,21 @@
+package atividadeslaa;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class GrafoTxt {
+
+    public static void main(String[] args) {
+        Scanner in;
+        try {
+            in = new Scanner(new FileReader("C:\\Users\\maxau\\Documents\\NetBeansProjects\\AtividadesLAA\\src\\atividadeslaa\\com-friendster.top5000.cmty.txt"));
+            while (in.hasNextLine()) {                
+                String line = in.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}

ff9813c0ba7314533daa73861545c786e2293610
maxau <maxau@192.168.0.3>
2020-05-26 19:48:50 -0300

    Atividade

--- src/atividadeslaa/GrafoTxt.java
+++ src/atividadeslaa/GrafoTxt.java
@@ -12,21 +12,21 @@ public class GrafoTxt {
     public static void main(String[] args) {
         Scanner in;
         LinkedList[] listas = new LinkedList[numeroVertice];
-        int[] numb = new int[5000];
+        String[] numb = new String[5000];
         int indice = 0;
         ligarListas(listas);
         try {
             in = new Scanner(new FileReader("com-friendster.top5000.cmty.txt"));
             while (in.hasNextLine()) {
-                int line = in.nextInt();
-                numb[indice] = line;
-
-                String lines = in.nextLine().trim();
-                listas[indice].add(lines);
-                
+                String line = in.nextLine();
+                String array[] = line.split("\t");
+                numb[indice] = array[0];
+                for (int i = 0; i < array.length - 2; i++) {
+                    listas[indice].add(array[i + 1]);
+                }
                 indice++;
             }
-            print(listas,numb);
+            print(listas, numb);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -38,11 +38,11 @@ public class GrafoTxt {
         }
     }
 
-    static void print(LinkedList[] listas,int numb[]) {
+    static void print(LinkedList[] listas, String numb[]) {
         for (int i = 0; i < numeroVertice; i++) {
-            System.out.print(numb[i]+": ");
+            System.out.print(numb[i] + ": ");
             for (int j = 0; j < listas[i].size(); j++) {
-                System.out.print(listas[i].get(j));
+                System.out.print(listas[i].get(j) + " ");
             }
             System.out.println();
         }

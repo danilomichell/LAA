6edc56bc38707bf79a622840abc825faced260a9
maxau <maxau@192.168.0.3>
2020-05-26 18:43:12 -0300

    Atividade

--- src/atividadeslaa/GrafoTxt.java
+++ src/atividadeslaa/GrafoTxt.java
@@ -12,16 +12,21 @@ public class GrafoTxt {
     public static void main(String[] args) {
         Scanner in;
         LinkedList[] listas = new LinkedList[numeroVertice];
+        int[] numb = new int[5000];
         int indice = 0;
         ligarListas(listas);
         try {
             in = new Scanner(new FileReader("com-friendster.top5000.cmty.txt"));
             while (in.hasNextLine()) {
-                String line = in.nextLine();
-                listas[indice].add(line);
+                int line = in.nextInt();
+                numb[indice] = line;
+
+                String lines = in.nextLine().trim();
+                listas[indice].add(lines);
+                
                 indice++;
             }
-            print(listas);
+            print(listas,numb);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -33,8 +38,9 @@ public class GrafoTxt {
         }
     }
 
-    static void print(LinkedList[] listas) {
+    static void print(LinkedList[] listas,int numb[]) {
         for (int i = 0; i < numeroVertice; i++) {
+            System.out.print(numb[i]+": ");
             for (int j = 0; j < listas[i].size(); j++) {
                 System.out.print(listas[i].get(j));
             }

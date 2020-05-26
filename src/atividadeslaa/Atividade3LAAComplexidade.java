6c31ac77a29549de937d305951a2cf5723518356
maxau <maxau@192.168.0.3>
2020-05-26 20:46:02 -0300

    Atividade

---
+++ src/atividadeslaa/Atividade3LAAComplexidade.java
@@ -0,0 +1,56 @@
+package atividadeslaa;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Atividade3LAAComplexidade {
+
+    static int numeroVertice = 30000;
+
+    public static void main(String[] args) {
+        Scanner in;
+        LinkedList[] listas = new LinkedList[numeroVertice];
+        String[] numb = new String[30000];
+        int indice = 0;
+        ligarListas(listas);
+        try {
+            in = new Scanner(new FileReader("grafo-25000.txt"));
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                String array[] = line.split("\t");
+                numb[indice] = array[0];
+                for (int i = 1; i < array.length; i++) {
+                    listas[indice].add(array[i]);
+                }
+                indice++;
+            }
+            print(listas, numb);
+            complexidade(listas);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void ligarListas(LinkedList[] listas) {
+        for (int i = 0; i < numeroVertice; i++) {
+            listas[i] = new LinkedList();
+        }
+    }
+
+    static void print(LinkedList[] listas, String numb[]) {
+        for (int i = 0; i < numeroVertice; i++) {
+            System.out.println(numb[i] + " tem " + listas[i].size() + " vértices adjacentes");
+            System.out.println();
+        }
+    }
+
+    static void complexidade(LinkedList[] listas) {
+        int complexidade = 0;
+        for (int i = 0; i < numeroVertice; i++) {
+            complexidade += listas[i].size() + (listas[i].size()+1);
+        }
+        System.out.println("O grafo possui complexidade "+complexidade);
+    }
+}
